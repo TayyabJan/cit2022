0972f73fe24774b7a5a50837a9825527c983416e
TayyabJan <tayyabjan@live.com>
2022-04-17 14:31:22 +0500

    practice_products completed

--- data-serialization/practice/practice_products.js
+++ data-serialization/practice/practice_products.js
@@ -5,7 +5,7 @@ const products = require('../data/products/products.json')
 // 1. Create a function that returns the name of the product with the highest name length.
 //    If there are two or more products with the same length, return the one with the lowest id.
 
-// // function that return lengths of titles 
+// // // function that return lengths of titles 
 const highestNamefn = (p =>  p.map( p => p.title).sort((a,b) => (a.length-b.length)));
 const highestNameList = highestNamefn(products);
 console.log(highestNameList);
@@ -15,7 +15,8 @@ let titleOfLowestLength = highestNameList[0];
 let titleOfHighestLength  = highestNameList[highestNameList.length-1];
 
 // // function that fetch titles with same highest length
-const getLowestfn = ( (p,value) => p.map( p => (p.title.length==value) ? p.title : null).filter(p => p!=null).sort((a,b) =>(a-b)));
+const getLowestfn = ( (p,value) => p.map( p => (p.title.length==value) ? p.title : null).filter(p => p!=null)
+.sort((a,b) =>(a-b)));
 
 titleOfLowestLength = getLowestfn(products,titleOfLowestLength.length);
 titleOfHighestLength = getLowestfn(products,titleOfHighestLength.length);
@@ -25,13 +26,76 @@ console.log( titleOfLowestLength[0] + " --- Lowest Length :" +  titleOfLowestLen
 console.log( titleOfHighestLength[0] + " --- Highest Length :"   + titleOfHighestLength[0].length);
  
  
-// 2. Create a function that returns the name of the product with the highest price.
-//    If there are two or more products with the same price, return the one with the lowest id.
+// 2. //Create a function that returns the name of the product with the highest price.
+////    If there are two or more products with the same price, return the one with the lowest id.
 
-// 3. Create a list of all the products that are from the same brand.
+ 
+// // // // function that return prices of titles 
+const highestpriceProduct = (p =>  p.map( p => p).sort((a,b) => (a.price-b.price)));
+const productslist = highestpriceProduct(products);
 
-// 4. Print the average price of the products.
 
-// 5. Create a list of all the products that have highest stock units.
+//console.log(productslist);
+
+// // checking prices
+//console.log(products.map ( p => p.price).sort( (a,b) => (a-b)));
+
+// // // save lowest and highest price titles. 
+let titlewithLowestPrice = productslist[0].price;
+let titleOfHighestPrice  = productslist[productslist.length-1].price;
+
+ 
+
+// // function that fetch titles with same highest price 
+const getLowestfn1 = ( (p,value) => 
+p.map( p => (p.price==value) ? p.title + " ( ID : " + p.id + " Price: " + p.price +" )" : null)
+.filter(p => p!=null).sort((a,b) =>(a.price-b.price)));
+
+titlewithLowestPrice = getLowestfn1(products,titlewithLowestPrice);
+
+titleOfHighestPrice = getLowestfn1(products,titleOfHighestPrice);
+
+ 
+// Print first Lowest and Highest prices based on ID .
+console.log( titlewithLowestPrice[0]);
+console.log( titleOfHighestPrice[0]);
 
 
+// 3//. Create a list of all the products that are from the same brand.
+      
+// // picking list of same categories 
+
+const categoryList = products.map(p =>p.category.trim());
+const sameCategoryList = categoryList.filter( (item, index ) => categoryList.indexOf(item)!=index);
+const categoryListSet = new Set(sameCategoryList);
+
+const productsWithSameCategory = products.map ( p => sameCategoryList.includes(p.category)? p : null)
+.filter(p=>p!=null);
+
+console.log(categoryListSet);
+console.log(productsWithSameCategory.map(p=>p.category)); 
+// this dont'y show the test category created last cz it has no many products
+
+
+
+//// 4. Print the average price of the products.
+
+const avgPrice = Math.round(products.map (p =>p.price).reduce((a,b) => (a+b))/products.length);
+console.log("Products Average Price is : " + avgPrice);
+
+//// 5. Create a list of all the products that have highest stock units.
+
+const products2 = require("../data/others/products.json");
+const highestStockUnits = products2.map(p =>p.stockUnits).sort( (a,b) =>(a-b)).reduce( (a,b) => (a>b)? a: b);
+console.log("Highest stock Value is : " + highestStockUnits);
+const productswithHighestStockUnits = products2.map(p => highestStockUnits ==(p.stockUnits)? p.name : null)
+.filter(p =>p!=null);
+ //console.log(productswithHighestStockUnits)
+
+const productbyStockValuefn = (stockvalue  => products2.map( p => p.stockUnits<=stockvalue ? p: null)
+.filter(p =>p!=null).sort( (a,b) => (a.stockUnits-b.stockUnits)));
+const stockvalue = 100;
+const productlistStockwise = productbyStockValuefn(stockvalue);
+
+console.log( `Products having Stock Value Greate or Equal to ${stockvalue} \n`);
+productlistStockwise.forEach(p => console.log(p.name + " ---- Stock :" + p.stockUnits));
